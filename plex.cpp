8b8731539657a64d73d47f79693dfdfeba16ac9e
unknown <unknown_@live.jp>
2014-12-20 11:49:56 +0900

    意味は無いけど気になったので…

--- plex.cpp
+++ plex.cpp
@@ -75,7 +75,7 @@ static const DWORD LookupTable1[256] = {
 static const DWORD LookupTable2[NUM_ROUND] = {
 	0x01000000, 0x02000000, 0x04000000, 0x08000000,
 	0x10000000, 0x20000000, 0x40000000, 0x80000000,
-	0x1B000000, 0x36000000
+	0x1b000000, 0x36000000
 };
 
 static void GetKey(BYTE *Key, BYTE *Seed, BYTE *Rand)
